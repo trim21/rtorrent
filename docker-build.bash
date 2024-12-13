f39fd7230c77c19e26071c94fa805df10345c54e
Trim21 <trim21.me@gmail.com>
2024-12-14 05:15:45 +0800

    test

--- docker-build.bash
+++ docker-build.bash
@@ -1,5 +1,5 @@
 if [[ $(uname -m) == "aarch64" ]]; then
-  sed -i 's/architecture = \"all\"/architecture = \"arm64\"/' BUILD.bazel;
+  sed -i 's/architecture = \"all\"/architecture = \"arm64\"/' BUILD.bazel
 elif [[ $(uname -m) == "x86_64" ]]; then
-  sed -i 's/architecture = \"all\"/architecture = \"amd64\"/' BUILD.bazel;
+  sed -i 's/architecture = \"all\"/architecture = \"amd64\"/' BUILD.bazel
 fi
